dc70aac2e22f1de54dd7b2f2eefcbd739f39f697
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-03 23:43:08 +0530

    Added styles to items.component.ts and updated imports

--- src/app/item/items.component.ts
+++ src/app/item/items.component.ts
@@ -2,12 +2,14 @@ import { Component, NO_ERRORS_SCHEMA, inject } from '@angular/core'
 import { NativeScriptCommonModule, NativeScriptRouterModule } from '@nativescript/angular'
 import { Page } from '@nativescript/core'
 import { ItemService } from './item.service'
+ 
 
 @Component({
   selector: 'ns-items',
   templateUrl: './items.component.html',
   imports: [NativeScriptCommonModule, NativeScriptRouterModule],
   schemas: [NO_ERRORS_SCHEMA],
+  styles: ['./items.component.css'],
 })
 export class ItemsComponent {
   itemService = inject(ItemService)
@@ -22,4 +24,5 @@ export class ItemsComponent {
       }
     })
   }
+  
 }
